e786fd9dde8fb0b7e8f9a35f91dcc29d363e8f31
Youngsun Noh <youngsunkr@hotmail.com>
2026-06-02 17:28:57 +0900

    레이아웃 구성중

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Header />
+      
       <body className="min-h-full flex flex-col">{children}</body>
+
+      <Footer />
     </html>
   );
 }
