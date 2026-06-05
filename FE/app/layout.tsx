0bfeda2c2ad4e8c7e3d3d09fd3ab9c32c3b9a20c
Youngsun Noh <youngsunkr@hotmail.com>
2026-06-05 22:34:37 +0900

    <Header /> 컴포넌트(또는 해당 위치에 포함된 UI 요소들)가 <body> 태그 내부로 들어가도록 수정

--- FE/app/layout.tsx
+++ FE/app/layout.tsx
@@ -31,11 +31,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Header />
 
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        
+        <Header />
+        
+        {children}
 
-      <Footer />
+        <Footer />
+      </body>
+
     </html>
   );
 }
