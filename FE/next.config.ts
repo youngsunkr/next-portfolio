f8f32a0fb6676f07d0377d6c3aab2e8c32af2bfd
Youngsun Noh <youngsunkr@hotmail.com>
2026-06-10 12:45:00 +0900

    리스트 출력 완료

--- FE/next.config.ts
+++ FE/next.config.ts
@@ -6,8 +6,50 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.200.194', 'localhost:3000'],
   reactStrictMode: true,
   images: {
-    domains: ['www.notion.so', 'www.gscaltex.com', 'www.kbinsure.co.kr', 'www.modetournetwork.com', 'images.unsplash.com'],
+    // domains: [
+    //   'www.notion.so', 
+    //   'images.unsplash.com', 
+    //   'www.gscaltex.com', 
+    //   'www.kbinsure.co.kr', 
+    //   'www.modetournetwork.com', 
+    //   'www.korloy.com', 
+    //   'www.lgensol.com'],
+    
+    //  최신 방식 (보안 강화)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.modetournetwork.com',
+        port: '',
+        pathname: '/**', // 해당 도메인의 모든 경로 허용
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kbinsure.co.kr',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gscaltex.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.korloy.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lgensol.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
+  
 };
 
 export default nextConfig;
